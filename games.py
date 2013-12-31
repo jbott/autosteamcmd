896f3648634e6143ebcc77b55f30b8320511db75
jbott <snowleopardjb@gmail.com>
2013-12-30 20:48:55 -0500

    Move listing of games into games.py file

--- games.py
+++ games.py
@@ -1,5 +1,11 @@
-import json
+import json, color
 
 def load_file(file):
 	f = open(file, 'r')
 	return json.load(f)
+
+def list_games(games):
+	name_width = max(len(row['name']) for row in games )# Padding for name
+	color.printcln("Name:".ljust(name_width) + '    ' + "App ID:".ljust(10) + '    ' + "Aliases:")
+	for game in games:
+		color.printcln(game['name'].ljust(name_width) + '    ' + str(game['app_id']).ljust(10) + '    ' + "".join(alias + ' ' for alias in game['aliases']))
